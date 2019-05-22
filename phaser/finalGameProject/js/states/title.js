bad400d156fce11cc089b1928318f743353b7527
Derek Jean <derekjean99@gmail.com>
2019-05-21 18:28:43 -0700

    Added button with in progress coffin opening animation

--- phaser/finalGameProject/js/states/title.js
+++ phaser/finalGameProject/js/states/title.js
@@ -4,23 +4,28 @@ var Title = function(game) {};
 Title.prototype = {
 	create: function() {
 		// add title screen text
-		var titleText = game.add.text(game.width/2, game.height/2, 'final Game Project', {font: 'Helvetica', fontSize: '48px', fill: '#fff'});
+		var titleText = game.add.text(game.width/2, game.height*.2, 'final Game Project', {font: 'Helvetica', fontSize: '48px', fill: '#fff'});
 		titleText.anchor.set(0.5);
 
-		var instructText = game.add.text(game.width/2, game.height/2 + 48, 'By Group 50\nDerek Jean, David Monroe, Kyle Fegan', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
+		var instructText = game.add.text(game.width/2, game.height*.3, 'By Group 50\nDerek Jean, David Monroe, Kyle Fegan', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
 		instructText.anchor.set(0.5);
 
-		var playText = game.add.text(game.width/2, game.height*.7, '2 player versus. Fast moving balls kill players. Last one standing wins.', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
+		var playText = game.add.text(game.width/2, game.height*.5, '2 player versus. Fast moving balls kill players. Last one standing wins.', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
 		playText.anchor.set(0.5);
 
-		var playText = game.add.text(game.width/2, game.height*.8, 'P1 = arrows keys to move, numpad 0 knocks balls\nP2 = awd keys to move, left shift knocks balls\n Press r to end, can restart after ending\nUp arrow starts', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
+		var playText = game.add.text(game.width/2, game.height*.6, 'P1 = arrows keys to move, numpad 0 knocks balls\nP2 = awd keys to move, left shift knocks balls\n Press r to end, can restart after ending', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
 		playText.anchor.set(0.5);
+
+		// creates play button
+		var button = game.add.button(game.width/2, game.height*.8, 'playbutton', this.goPlay, this,1, 0, 2);
+		button.anchor.set(0.5);
 	},
 	update: function() {
-		// check for UP input
-		if(game.input.keyboard.justPressed(Phaser.Keyboard.UP)) {
-			//clear world: true, clear cache: false, freshStart: true, playerOneLives: 0 (will be overwritten), playerTwoLives: 0 (will be overwritten)
+	},
+	goPlay: function() {
+		//clear world: true, clear cache: false, freshStart: true, playerOneLives: 0 (will be overwritten), playerTwoLives: 0 (will be overwritten)
+		//game.time.events.add(Phaser.Timer.SECOND * 2, function() {
 			game.state.start('Play', true, false, true, 0, 0);
-		}
+		//});
 	}
 };
