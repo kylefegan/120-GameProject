5888cefe7d54b80bfb8273e310057847886fecb9
Derek Jean <derekjean99@gmail.com>
2019-06-09 10:21:09 -0700

    Stopped restart crash

--- phaser/finalGameProject/js/states/gameover.js
+++ phaser/finalGameProject/js/states/gameover.js
@@ -13,7 +13,7 @@ GameOver.prototype = {
 			/*var playText = game.add.text(game.width/2, game.height*.4, 'Player 2 Wins', {font: 'Helvetica', fontSize: '48px', fill: '#fff'});
 			playText.anchor.set(0.5);*/
 			var playText = game.add.text(game.width*.27, game.height*.15, 'Press R to Restart', {font: 'Palatino', fontSize: '24px', fill: '#000'});
-		} else if (this.p2Lives == 0) {
+		} else {
 			var EndScreen = game.add.sprite(0,0, 'End1');
 			/*var playText = game.add.text(game.width/2, game.height*.4, 'Player 1 Wins', {font: 'Helvetica', fontSize: '48px', fill: '#fff'});
 			playText.anchor.set(0.5);*/
