453671d1eb40419a34a48253be37486654d3838e
DavidJMonroe <djmonroe@ucsc.edu>
2019-06-11 20:10:02 -0700

    Reverted mistakenly changed control screen text
    The specific string text used in the controls game state got overwritten with an older version. Should be remedied now.

--- phaser/finalGameProject/js/states/controls.js
+++ phaser/finalGameProject/js/states/controls.js
@@ -9,10 +9,14 @@ Controls.prototype = {
 		controlsAnimate.animations.add('cycle', [0,1,2,3,4,5,6,7,8], 1, true);
 		controlsAnimate.animations.play('cycle');
 
-		var playText = game.add.text(game.width/2, game.height*.1, 'Fast moving objects kill players. Last one standing wins.', {font: 'Palatino', fontSize: '30px', fill: '#000'});
+		var playText = game.add.text(game.width/2, game.height*.14, 'Players cant hit each other directly, only fast moving objects kills. There \nis destructible pillars on the stage that can be attacked. All players can \ndouble jump and fast fall. Last one standing wins.', {font: 'Palatino', fontSize: '24px', fill: '#000'});
 		playText.anchor.set(0.5);
 
-		var playText = game.add.text(game.width/2, game.height*.2, 'P1 = wasd keys to move, "c" knocks objects, "v" creates a shield\nP2 = arrow keys to move, "," knocks objects, "." creates a shield\nPress "r" to end the game                                                                       Play-', {font: 'Palatino', fontSize: '24px', fill: '#000'});
+		var playText = game.add.text(game.width/2, game.height*.24, 'P1 = wasd keys to move, "c" knocks objects, "v" creates a shield, Press "r" to end the game\nP2 = arrow keys to move, "," knocks objects, "." creates a shield                               Play-', {font: 'Palatino', fontSize: '20px', fill: '#000'});
+		playText.anchor.set(0.5);
+
+		nextButton = game.add.button(game.width*.915, game.height*.24, 'next', this.goNext, this,1, 0, 2);
+
 		playText.anchor.set(0.5);
 
 		nextButton = game.add.button(game.width*.915, game.height*.24, 'next', this.goNext, this,1, 0, 2);
